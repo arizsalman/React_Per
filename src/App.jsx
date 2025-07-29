779c519616021c18cbbe99c59fe45d9dfe5f2d50
arizsalman <muhammadarizsalman@gmail.com>
2025-07-29 21:04:29 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,26 @@ function App() {
   // const [isSent, setIsSent] = useState(false)
   // const [message, setMessage] = useState('Here!')
 
+   function Form() {
+    const [person, setPerson] = useState({
+      firstName: 'Barbara',
+      lastName: 'Hepworth',
+      email: 'bhepworth@sculpture.com'
+    });
+  
+    function handleFirstNameChange(e) {
+      person.firstName = e.target.value;
+    }
+  
+    function handleLastNameChange(e) {
+      person.lastName = e.target.value;
+    }
+  
+    function handleEmailChange(e) {
+      person.email = e.target.value;
+    }
+  
+
   async function handleClick() {
     setPending(p => p + 1)
     await delay(2000);
@@ -31,13 +51,41 @@ function App() {
     )
   }
 
-
+}
   return (
 
 
 
     <div>
 
+      <div>
+        <label>
+        First name:
+        <input
+          value={person.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={person.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          value={person.email}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <p>
+        {person.firstName}{' '}
+        {person.lastName}{' '}
+        ({person.email})
+      </p>
+      </div>
       {/* <form onSubmit={(e) => (
         e.preventDefault(),
         setIsSent(true),
@@ -75,4 +123,4 @@ function App() {
   }
 }
 
-export default App
+export default App 
